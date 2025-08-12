79f52bb06c2dd9fda31aadc0b2f13201a6188491
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-12 04:17:17 -0300

    refactor solution

--- two-pointers/remove-duplicates-from-sorted-array/Main.java
+++ two-pointers/remove-duplicates-from-sorted-array/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         final var solution = new Solution();
-        solution.removeDuplicates(new int[] {1,2,2});
+        solution.removeDuplicates(new int[] {1,2,2,3,3,3,4});
     }
 }
--- two-pointers/remove-duplicates-from-sorted-array/Solution.java
+++ two-pointers/remove-duplicates-from-sorted-array/Solution.java
@@ -1,35 +1,15 @@
-import java.util.Arrays;
-
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 1) {
-            return 1;
+        if (nums.length == 0) {
+            return 0;
         }
-        int l = 0, r = 1, n = nums.length - 1;
-        boolean isDuplicationFound = false;
-        while (l < r) {
-            System.out.println(Arrays.toString(nums));
-            if (nums[l] == nums[r]) {
-               isDuplicationFound = true;
-               if (r < n) {
-                    r++;
-                } else {
-                    return l + 1;
-                }
-            } else if (isDuplicationFound) {
-                int tmp = nums[l + 1];
-                nums[l + 1] = nums[r];
-                nums[r] = tmp;
-                l++;
-                if (r < n) {
-                    r++;
-                } else {
-                    return l + 1;
-                }
-            } else {
-                l++;
+        int slow = 0;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
             }
         }
-        return nums.length;
+        return slow + 1;
     }
 }
