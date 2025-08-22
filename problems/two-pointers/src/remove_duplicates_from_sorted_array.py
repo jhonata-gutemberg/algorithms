22293235ab9d95db5ab80bcc9a13a850528ac1a3
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-22 05:20:38 -0300

    solving remove duplicates in python

---
+++ problems/two-pointers/src/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        write = 0
+        for read in range(1, len(nums)):
+            if nums[read] != nums[write]:
+                write += 1
+                nums[write] = nums[read]
+        return write + 1
