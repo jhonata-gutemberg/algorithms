167563b50054da5749bf55cb813e4bc01bf3ecb7
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-09-01 19:34:04 -0300

    solving valid palindrome

--- problems/two-pointers/resolutions/valid_palindrome.py
+++ problems/two-pointers/resolutions/valid_palindrome.py
@@ -1,12 +1,13 @@
-def is_palindrome_valid(s: str) -> bool:
-    l = 0
-    r = len(s) - 1
+def isPalindrome(self, s: str) -> bool:
+    l, r = 0, len(s) - 1
     while l < r:
-        while l < r and not s[l].isalnum():
+        if not s[l].isalnum():
             l += 1
-        while l < r and not s[r].isalnum():
+            continue
+        if not s[r].isalnum():
             r -= 1
-        if s[l] != s[r]:
+            continue
+        if s[l].lower() != s[r].lower():
             return False
         l += 1
         r -= 1
