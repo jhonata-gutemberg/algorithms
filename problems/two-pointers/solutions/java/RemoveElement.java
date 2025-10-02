be65cbc64e2db192ac5b17fbdfa1278e85a22b3c
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-10-02 14:47:39 -0300

    add remove element solution

---
+++ problems/two-pointers/solutions/java/RemoveElement.java
@@ -0,0 +1,13 @@
+package java;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != val) {
+                nums[l++] = nums[r];
+            }
+        }
+        return l;
+    }
+}
