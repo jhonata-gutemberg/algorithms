7e572f6fe1c092387bcad48c2dd2c69b4c630e07
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-10-03 08:04:34 -0300

    add java solution to reverse string

---
+++ problems/two-pointers/solutions/java/ReverseString.java
@@ -0,0 +1,15 @@
+package java;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
